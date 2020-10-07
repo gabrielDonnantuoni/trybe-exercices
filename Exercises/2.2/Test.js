33ba3462c3696657b81618283f1019b3e58c7b6e
Gabriel Donnantuoni Lima <gabrieldonnantuoni@gmail.com>
2020-10-07 18:33:54 -0300

    Verificar o ṕull novamente

--- Exercises/2.2/Test.js
+++ Exercises/2.2/Test.js
@@ -3,4 +3,5 @@ let studentSkills = 10;
 // Eu vou ter muito sucesso na programação!
 // Eu sou Tryber e feliz.
 // Verificar o push novamente
+// Verificar o pull novamente
 console.log("Sucesso!");
