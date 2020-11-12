f1d35bb3729a4b6eb0f01b754061d9aa817353fe
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2020-11-12 16:24:24 -0300

    Updating block 5

--- 1-Web_Dev_Fundamentals/block_4/day_1/s8-number_sign.js
+++
@@ -1,9 +0,0 @@
-let a = -3;
-
-if (a > 0){
-    console.log(a + " is the positive");
-} else if (a < 0) {
-    console.log(a + " is the negative");
-}else {
-    console.log(a + " is equal to 0");
-}
